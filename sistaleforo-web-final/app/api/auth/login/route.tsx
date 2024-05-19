d4f6e7337a6aa83a7320f025da3b417643ba1c09
JoseLuisADA <joseluis.luzuriagaalanis.web@adaits.es>
2024-05-19 12:36:43 +0200

    useless variable removed and comments removed

--- sistaleforo-web-final/app/api/auth/login/route.tsx
+++ sistaleforo-web-final/app/api/auth/login/route.tsx
@@ -1,9 +1,6 @@
 //app/auth/login/route.tsx
 import axios from '../../../../axios';
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_SERVER_URL; // Asegúrate de tener esta variable en tu .env
-
-// Función para realizar el login
 export const login = async (username: string, password: string): Promise<any> => {
   try {
     const response = await axios.post(`/login`, { username, password });
