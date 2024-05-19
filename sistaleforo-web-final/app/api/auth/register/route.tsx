67a5044d102668feec74495c0efbbd430162ccf8
JoseLuisADA <joseluis.luzuriagaalanis.web@adaits.es>
2024-05-20 00:48:16 +0200

    api front register new features

--- sistaleforo-web-final/app/api/auth/register/route.tsx
+++ sistaleforo-web-final/app/api/auth/register/route.tsx
@@ -1,11 +1,24 @@
 //app/auth/register/route.tsx
 import axios from '../../../../axios';
+import { isAxiosError } from 'axios';
 
 export const register = async (username: string, password: string): Promise<any> => {
   try {
     const response = await axios.post('/register', { username, password });
+    console.log('Response:', response);
     return response.data;
-  } catch (error) {
-    throw new Error(error.response?.data.message || 'Error al crear la cuenta');
+  } catch (error: any) {
+    if (isAxiosError(error)) { 
+      if (error.response) {
+        const message = error.response.data.Mensaje || 'Error desconocido al intentar registrarse';
+        throw new Error(message);
+      } else if (error.request) {
+        throw new Error('No se recibió respuesta del servidor');
+      } else {
+        throw new Error('Error al configurar la solicitud');
+      }
+    } else {
+      throw new Error('Error desconocido al intentar registrarse ' + error.message);
+    }
   }
 };
