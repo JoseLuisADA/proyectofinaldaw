39606751bdb6fed2c08fa6398d22d7e565e0bd29
JoseLuisADA <joseluis.luzuriagaalanis.web@adaits.es>
2024-05-20 00:10:35 +0200

    Register new features

--- sistaleforo-web-final/app/register/page.tsx
+++ sistaleforo-web-final/app/register/page.tsx
@@ -1,11 +1,12 @@
 'use client';
 
 import React, { useState, useContext } from 'react';
-import { useRouter } from 'next/navigation'; // Usar el enrutador de navegación de Next.js
-import { AuthContext } from '../../components/AuthContext'; // Ajusta la ruta según la estructura de tu proyecto
+import { useRouter } from 'next/navigation';
+import { AuthContext } from '../../components/AuthContext';
+import userSchema from '../lib/userSchema';
 
 const SignUpPage = () => {
-  const { error, isLoading } = useContext(AuthContext);
+  const { register, error, isLoading } = useContext(AuthContext);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
@@ -14,34 +15,26 @@ const SignUpPage = () => {
 
   const handleSignUp = async (event) => {
     event.preventDefault();
-    if (password !== confirmPassword) {
-      setValidationError('Las contraseñas no coinciden');
+
+    const result = userSchema.safeParse({ username, password, confirmPassword });
+
+    if (!result.success) {
+      setValidationError(result.error.issues.map(issue => issue.message).join(", "));
       return;
     }
+
     setValidationError('');
 
     try {
-      const response = await fetch('/api/auth/register', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ username, password }),
-      });
-
-      if (response.ok) {
-        router.push('/login'); // Redirige al usuario a la página de inicio de sesión después del registro exitoso
-      } else {
-        const errorData = await response.json();
-        setValidationError(errorData.message || 'Error al crear la cuenta');
-      }
+      const msg = await register(username, password);
+      router.push('/login'); // Redirige al usuario a la página de inicio de sesión después del registro exitoso
     } catch (err) {
-      setValidationError('Error durante el registro');
     }
   };
 
   return (
     <div className="w-full h-full">
+      <h2 className='text-center'>Registro</h2>
       <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSignUp}>
         <div className="mb-4">
           <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
@@ -55,6 +48,7 @@ const SignUpPage = () => {
             value={username}
             onChange={(e) => setUsername(e.target.value)}
           />
+          <p className="text-gray-600 text-xs italic">Debe ser único y contener al menos 1 carácter.</p>
         </div>
         <div className="mb-6">
           <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
@@ -68,6 +62,7 @@ const SignUpPage = () => {
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
+          <p className="text-gray-600 text-xs italic">Debe tener al menos 1 caracter.</p>
         </div>
         <div className="mb-6">
           <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="confirmPassword">
@@ -81,6 +76,7 @@ const SignUpPage = () => {
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
           />
+          <p className="text-gray-600 text-xs italic">Debe coincidir con la contraseña.</p>
         </div>
         <div className="flex items-center justify-between">
           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
