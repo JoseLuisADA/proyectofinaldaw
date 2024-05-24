59afb716472916aa27b310fbca7541c813234c34
JoseLuisADA <joseluis.luzuriagaalanis.web@adaits.es>
2024-05-24 12:30:53 +0200

    New UserContext file

---
+++ sistaleforo-web-final/src/context/UserContext.tsx
@@ -0,0 +1,40 @@
+'use client'
+import React, { createContext, useContext, useState } from "react"
+import { UserProps } from "../types/user"
+
+export interface UserContextType {
+  user: { username: string }
+  setUser: React.Dispatch<React.SetStateAction<UserProps>>
+  handleSetUserUsername: (username: string) => void
+}
+
+const UserContext = createContext<UserContextType | undefined>(undefined)
+
+export const UserProvider = ({ children }: { children: React.ReactNode }) => {
+  
+  const [user, setUser] = useState<UserProps>({
+    username: "",
+  })
+
+  const handleSetUserUsername = (username: string) => {
+    setUser((prevUser) => ({
+      ...prevUser,
+      username,
+    }))
+  }
+
+  return (
+    <UserContext.Provider
+      value={{ user, setUser, handleSetUserUsername}}>
+      {children}
+    </UserContext.Provider>
+  )
+}
+
+export const useUserContext = () => {
+  const context = useContext(UserContext)
+  if (!context) {
+    throw new Error("useUser must be used within a UserProvider")
+  }
+  return context
+}
