6d0e24cd8b4de2c0530ad71e013f7845b2db742a
JoseLuisADA <joseluis.luzuriagaalanis.web@adaits.es>
2024-05-24 12:19:37 +0200

    New API Request Login

---
+++ sistaleforo-web-final/src/app/api/auth/login/route.tsx
@@ -0,0 +1,33 @@
+"use server"
+import { NextRequest, NextResponse } from "next/server";
+import axios from "axios";
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+
+  try {
+      await axios.post(
+      `${process.env.NEXT_PUBLIC_API_SERVER_URL}/login`,
+      {
+        username: body.username,
+        password: body.password,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        withCredentials: true, // Permitir el uso de cookies en la solicitud
+      }
+    );
+
+    return NextResponse.json({ message: 'Se ha realizado login con éxito' });
+  } catch (error: unknown) {
+    console.log(error);
+    if (axios.isAxiosError(error)) {
+      if (error.response && error.response.status === 401) {
+        return NextResponse.json({ message: "Credenciales inválidas" }, { status: 401 });
+      }
+    }
+    return NextResponse.json({ message: "Error interno" }, { status: 500 });
+  }
+}
