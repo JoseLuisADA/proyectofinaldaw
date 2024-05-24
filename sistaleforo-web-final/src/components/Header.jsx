9021b4af052998f172ab118f0ebbd2b02eaa86b3
JoseLuisADA <joseluis.luzuriagaalanis.web@adaits.es>
2024-05-24 13:49:52 +0200

    Fix extension component header

--- sistaleforo-web-final/src/components/Header.jsx
+++ sistaleforo-web-final/src/components/Header.jsx
@@ -1,13 +1,14 @@
 'use client';
 import React from 'react';
+import NavBar from './NavBar';
 
 const Header = () => (
-  <footer className="bg-light p-3 text-center" data-testid="header">
-    <div className="logo" data-testid="header-logo" />
+  <header className="bg-light p-3 text-center" data-testid="header">
     <h1 data-testid="header-text">
       Sistale
     </h1>
-  </footer>
+    <NavBar />
+  </header>
 );
 
 export default Header;
