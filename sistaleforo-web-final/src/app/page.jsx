0612a5069d7f3066f070bc20569b18418b0ad589
JoseLuisADA <joseluis.luzuriagaalanis.web@adaits.es>
2024-05-24 12:18:22 +0200

    app/page.jsx modified

---
+++ sistaleforo-web-final/src/app/page.jsx
@@ -0,0 +1,12 @@
+//sistaleforo-web-final/app/page.jsx
+'use client';
+
+import React from 'react';
+import ArticulosList from '../components/ArticulosList';
+
+
+export default function Index() {
+  return (
+      <ArticulosList/>
+  );
+}
