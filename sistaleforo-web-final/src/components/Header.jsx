46ebc62b2a406e364b1b8c2798c7a6619d8a39f5
JoseLuisADA <joseluis.luzuriagaalanis.web@adaits.es>
2024-05-24 12:24:23 +0200

    New Component Header

---
+++ sistaleforo-web-final/src/components/Header.jsx
@@ -0,0 +1,13 @@
+'use client';
+import React from 'react';
+
+const Header = () => (
+  <footer className="bg-light p-3 text-center" data-testid="header">
+    <div className="logo" data-testid="header-logo" />
+    <h1 data-testid="header-text">
+      Sistale
+    </h1>
+  </footer>
+);
+
+export default Header;
