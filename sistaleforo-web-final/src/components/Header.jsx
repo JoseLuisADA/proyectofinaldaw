aed0b5b95951f2e68e29c7456fe76cdaab231ecd
JoseLuisADA <joseluis.luzuriagaalanis.web@adaits.es>
2024-05-25 13:38:20 +0200

    Sistale title in header renamed to Sistale Foro

--- sistaleforo-web-final/src/components/Header.jsx
+++ sistaleforo-web-final/src/components/Header.jsx
@@ -5,7 +5,7 @@ import NavBar from './NavBar';
 const Header = () => (
   <header className="bg-light p-3 text-center" data-testid="header">
     <h1 data-testid="header-text">
-      Sistale
+      Sistale Foro
     </h1>
     <NavBar />
   </header>
