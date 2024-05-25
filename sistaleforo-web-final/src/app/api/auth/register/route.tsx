6eaf027f936cc5021e764d27b0389950c10230ff
JoseLuisADA <joseluis.luzuriagaalanis.web@adaits.es>
2024-05-25 13:31:08 +0200

    axios imports in register api, fixed

--- sistaleforo-web-final/src/app/api/auth/register/route.tsx
+++ sistaleforo-web-final/src/app/api/auth/register/route.tsx
@@ -1,6 +1,8 @@
 "use server"
 import { NextRequest, NextResponse } from "next/server";
-import axios from "axios";
+import axios from "../../../../../axios";
+import { isAxiosError } from 'axios';
+
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
@@ -23,10 +25,10 @@ export async function POST(req: NextRequest) {
     return NextResponse.json({ message: 'Cuenta creada' }, { status: 201 });
   } catch (error: unknown) {
     console.log(error);
-    if (axios.isAxiosError(error)) {
+    if (isAxiosError(error)) {
       if (error.response) {
         if (error.response.status === 409) {
-          return NextResponse.json({ message: "El usuario ya está creado" }, { status: 409 });
+          return NextResponse.json({ message: "El usuario ya existe" }, { status: 409 });
         } else if (error.response.status === 400) {
           return NextResponse.json({ message: error.response.data.message || "Solicitud incorrecta" }, { status: 400 });
         }
