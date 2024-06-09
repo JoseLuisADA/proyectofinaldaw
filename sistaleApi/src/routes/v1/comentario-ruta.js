17f62fe3e403a2e831da02cffafe2cea00bf077f
JoseLuisADA <joseluis.luzuriagaalanis.web@adaits.es>
2024-06-09 19:31:37 +0200

    New routes in comentario-ruta.js SistaleApi

--- sistaleApi/src/routes/v1/comentario-ruta.js
+++ sistaleApi/src/routes/v1/comentario-ruta.js
@@ -1,15 +1,17 @@
 // src/routes/v1/comentario-ruta.js
 import express from 'express';
+import * as articuloComentarioMiddleware from '../../middlewares/articulo-comentario-middleware.js';
 import * as comentarioControlador from '../../controllers/comentario-controlador.js';
 import { authMiddleware } from '../../middlewares/auth-middleware.js';
-import * as articuloComentarioMiddleware from '../../middlewares/articulo-comentario-middleware.js';
+import { checkAdminPermissions } from '../../middlewares/checkAdminPermissions-middleware.js';
+import { checkMemberPermissions } from '../../middlewares/checkMemberPermissions-middleware.js';
 
 const router = express.Router();
 
-router.post('/v1/comentario', authMiddleware, articuloComentarioMiddleware.validateCreateComentario, comentarioControlador.create);
-router.get('/v1/comentario/:idComentario', articuloComentarioMiddleware.validateGetAndDel, comentarioControlador.get);
-router.patch('/v1/comentario/:idComentario', authMiddleware, articuloComentarioMiddleware.validateUpdateComentario, comentarioControlador.update);
-router.delete('/v1/comentario/:idComentario', authMiddleware, articuloComentarioMiddleware.validateGetAndDel, comentarioControlador.del);
+router.post('/v1/comentario', authMiddleware, checkMemberPermissions, articuloComentarioMiddleware.validateCreateComentario, comentarioControlador.create);
+router.get('/v1/comentario/:idComentario', articuloComentarioMiddleware.validateGetAndDelComentario, comentarioControlador.get);
+router.patch('/v1/comentario/:idComentario', authMiddleware, checkAdminPermissions, articuloComentarioMiddleware.validateUpdateComentario, comentarioControlador.update);
+router.delete('/v1/comentario/:idComentario', authMiddleware, checkAdminPermissions, articuloComentarioMiddleware.validateGetAndDelComentario, comentarioControlador.del);
 router.get('/v1/comentarios/:idArticulo', comentarioControlador.list);
 
 export default router;
