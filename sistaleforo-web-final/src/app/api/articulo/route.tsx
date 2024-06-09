ad39cfd91da7f125bdd191273ed8af68334d56c1
JoseLuisADA <joseluis.luzuriagaalanis.web@adaits.es>
2024-06-09 19:41:36 +0200

    New routes .tsx added

--- sistaleforo-web-final/src/app/api/articulo/route.tsx
+++ sistaleforo-web-final/src/app/api/articulo/route.tsx
@@ -1,6 +1,6 @@
+// sistaleforo-web-final/src/app/api/articulo/route.tsx
 "use server"
-import axios from '../../../../axios';
-import { isAxiosError } from 'axios';
+import axiosInstance from '../../../../axios';  // Importa la instancia de Axios configurada
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest) {
@@ -9,24 +9,40 @@ export async function GET(req: NextRequest) {
   const size = searchParams.get('size') || '10';
 
   try {
-    const { data } = await axios.get(
-      `${process.env.NEXT_PUBLIC_API_SERVER_URL}/articulos`,
-      {
-        params: { page, size },
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        withCredentials: true, // Permitir el uso de cookies en la solicitud
-      }
-    );
+    const { data } = await axiosInstance.get(`/articulos`, {
+      params: { page, size }
+    });
 
     return NextResponse.json(data);
   } catch (error) {
-    if (isAxiosError(error)) {
-      if (error.response) {
-        return new Response(error.response.data.message || 'Error desconocido al obtener los artículos', { status: error.response.status });
-      }
-    }
-    return new Response('Error interno', { status: 500 });
+    console.error('Error al obtener los artículos:', error);
+    const status = error.response?.status || 500;
+    const message = error.response?.data.message || 'Error interno del servidor';
+    return new Response(message, { status });
   }
 }
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+
+  try {
+    const { data } = await axiosInstance.post(`/articulo`, {
+      titulo: body.titulo,
+      contenido: body.contenido,
+      username: body.username,
+    }, {
+      headers: {
+        'Authorization': `${req.headers.get('Authorization')}`
+      }
+    });
+
+    return NextResponse.json(data, { status: 201 });
+  } catch (error) {
+    console.error('Error al crear el artículo:', error);
+    const status = error.response?.status || 500;
+    const message = error.response?.data.message || 'Error desconocido al crear el artículo';
+    return new Response(message, { status });
+  }
+}
+
+
