2d5b3711108280531f53d1217ec699cc755fd3e4
JoseLuisADA <joseluis.luzuriagaalanis.web@adaits.es>
2024-06-09 19:55:01 +0200

    upgraded registerSchema

--- sistaleforo-web-final/src/lib/registerSchema.ts
+++ sistaleforo-web-final/src/lib/registerSchema.ts
@@ -3,7 +3,8 @@ import { z } from 'zod';
 const registerSchema = z.object({
     username: z.string().min(1, "El nombre de usuario es requerido."),
     password: z.string().min(1, "La contraseña debe tener al menos 1 caracter."),
-    confirmPassword: z.string()
+    confirmPassword: z.string(),
+    email: z.string().email("El email no es válido."),
   }).refine(data => data.password === data.confirmPassword, {
     message: "Las contraseñas no coinciden",
     path: ["confirmPassword"]
