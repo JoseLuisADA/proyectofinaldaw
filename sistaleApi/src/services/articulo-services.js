dedadfce56f90ed9b1a4b923b4c139c52b7c4d8a
JoseLuisADA <joseluis.luzuriagaalanis.web@adaits.es>
2024-06-09 19:25:17 +0200

    upgraded articulo-services.js

--- sistaleApi/src/services/articulo-services.js
+++ sistaleApi/src/services/articulo-services.js
@@ -7,32 +7,35 @@ export async function createArticulo(titulo, contenido, username) {
     titulo,
     contenido,
     username: username
-  });
-  await articulo.save();
-  return articulo;
+  })
+  if(articulo.titulo === '' || articulo.contenido === '' || articulo.username === '') throw SistaleError.badRequest('Asegurate de que todos los campos esten rellenos')
+  await articulo.save()
+  return articulo
 }
 
 export async function getArticuloById(id) {
-   const articulo = await ArticuloModel.findById(id);
+   const articulo = await ArticuloModel.findById(id)
+  if (!articulo) throw SistaleError.badRequest('Artículo no encontrado')
    return articulo;
 }
 
 export async function updateArticulo(id, titulo, contenido) {
-  const articulo = await ArticuloModel.findByIdAndUpdate(id, { titulo, contenido }, { new: true });
-  if (!articulo) throw new Error('Artículo no encontrado');
-  return articulo;
+  const fecha = new Date()
+  const articulo = await ArticuloModel.findByIdAndUpdate(id, { titulo, contenido, fecha }, { new: true })
+  if (!articulo) throw SistaleError.badRequest('Artículo no encontrado')
+  return articulo
 }
 
 export async function deleteArticulo(id) {
-  const articulo = await ArticuloModel.findByIdAndDelete(id);
-  if (!articulo) throw new Error('Artículo no encontrado');
-  return articulo;
+  const articulo = await ArticuloModel.findByIdAndDelete(id)
+  if (!articulo) throw SistaleError.badRequest('Artículo no encontrado')
+  return articulo
 }
 
 export async function listArticulos(page, size) {
   const articulos = await ArticuloModel.find()
     .limit(size)
-    .skip(size * (page - 1));
-  const total = await ArticuloModel.countDocuments();
-  return { total, articulos };
+    .skip(size * (page - 1))
+  const total = await ArticuloModel.countDocuments()
+  return { total, articulos }
 }
