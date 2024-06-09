d05f5cb8972c4b0332b96edcd06559cfa0cb3ca9
JoseLuisADA <joseluis.luzuriagaalanis.web@adaits.es>
2024-06-09 20:03:49 +0200

    Port changed to 3001 i n SistaleAPI

--- sistaleApi/src/index.js
+++ sistaleApi/src/index.js
@@ -2,6 +2,6 @@
 import app from './app.js';
 import 'dotenv/config';
 
-export default app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
+export default app.listen(3001, () => {
+    console.log("Server is listening on port 3001");
 });
