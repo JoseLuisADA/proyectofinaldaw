67b6abcde33e9f24ee0c6cc996ef0303399815de
JoseLuisADA <joseluis.luzuriagaalanis.web@adaits.es>
2024-06-09 19:44:20 +0200

    ComentariosList component upgraded

--- sistaleforo-web-final/src/components/ComentariosList.tsx
+++ sistaleforo-web-final/src/components/ComentariosList.tsx
@@ -1,23 +1,32 @@
-// src/components/ComentariosList.tsx
+//sistaleforo-web-final/src/components/ComentariosList.tsx
 'use client';
 import React from 'react';
-import useComentarios from '../hooks/useComentarios';
+import PropTypes from 'prop-types';
+import useComentarios from '../hooks/comentario/useComentarios';
 import { ComentarioProps } from '../types/comentarioProps';
+import DeleteComentarioButton from './DeleteComentarioButton';
 
-interface ComentariosListProps {
-  idArticulo: string;
-}
+const ComentariosList = ({ idArticulo, user }) => {
+  const { comentarios, isLoading, refetch } = useComentarios(idArticulo);
 
-const ComentariosList: React.FC<ComentariosListProps> = ({ idArticulo }) => {
-  const { comentarios, isLoading } = useComentarios(idArticulo);
+  const handleCommentDeleted = () => {
+    refetch();
+  };
 
   return (
     <div>
       {isLoading && <p className='text-center'>Cargando comentarios...</p>}
-      {!isLoading &&  (
+      {!isLoading && (
         <ul>
           {comentarios.map((comentario: ComentarioProps) => (
             <li key={comentario._id} className="mb-4 p-4 border rounded shadow-sm">
+              {user.role === 'admin' && (
+                <DeleteComentarioButton
+                  idComentario={comentario._id}
+                  token={user.token}
+                  onCommentDeleted={handleCommentDeleted}
+                />
+              )}
               <p className="text-gray-700">{comentario.contenido}</p>
               <p className="text-sm text-gray-500">Por: {comentario.username}</p>
               <p className="text-sm text-gray-500">Fecha: {new Date(comentario.fecha).toLocaleString()}</p>
@@ -27,6 +36,11 @@ const ComentariosList: React.FC<ComentariosListProps> = ({ idArticulo }) => {
       )}
     </div>
   );
+};
+
+ComentariosList.propTypes = {
+  idArticulo: PropTypes.string.isRequired,
+  user: PropTypes.object.isRequired,
 };
 
 export default ComentariosList;
