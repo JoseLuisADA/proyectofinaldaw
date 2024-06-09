b9a54ce0ff6229294f29590e2eba450b967279bf
JoseLuisADA <joseluis.luzuriagaalanis.web@adaits.es>
2024-06-09 19:30:38 +0200

    upgraded articulo-ruta.js

--- sistaleApi/src/routes/v1/articulo-ruta.js
+++ sistaleApi/src/routes/v1/articulo-ruta.js
@@ -1,16 +1,17 @@
 // src/routes/v1/articulo-ruta.js
 import express from 'express';
 import * as articuloControlador from '../../controllers/articulo-controlador.js';
-import { authMiddleware } from '../../middlewares/auth-middleware.js';
 import * as articuloComentarioMiddleware from '../../middlewares/articulo-comentario-middleware.js';
+import { authMiddleware } from '../../middlewares/auth-middleware.js';
+import { checkAdminPermissions } from '../../middlewares/checkAdminPermissions-middleware.js';
 
 
 const router = express.Router();
 
 router.get('/v1/articulos', articuloControlador.list);
-router.get('/v1/articulo/:idArticulo', articuloComentarioMiddleware.validateGetAndDel, articuloControlador.get);
-router.post('/v1/articulo', authMiddleware, articuloComentarioMiddleware.validateCreateArticulo, articuloControlador.create);
-router.patch('/v1/articulo/:idArticulo', authMiddleware, articuloComentarioMiddleware.validateUpdateArticulo, articuloControlador.update);
-router.delete('/v1/articulo/:idArticulo', authMiddleware, articuloComentarioMiddleware.validateGetAndDel, articuloControlador.del);
+router.get('/v1/articulo/:idArticulo', articuloComentarioMiddleware.validateGetAndDelArticulo, articuloControlador.get);
+router.post('/v1/articulo', authMiddleware, checkAdminPermissions, articuloComentarioMiddleware.validateCreateArticulo, articuloControlador.create);
+router.patch('/v1/articulo/:idArticulo', authMiddleware, checkAdminPermissions, articuloComentarioMiddleware.validateUpdateArticulo, articuloControlador.update);
+router.delete('/v1/articulo/:idArticulo', authMiddleware, checkAdminPermissions, articuloComentarioMiddleware.validateGetAndDelArticulo, articuloControlador.del);
 
 export default router;
