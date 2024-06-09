87219ba658ccf3e66da6d7d631bc8b214c204bd9
JoseLuisADA <joseluis.luzuriagaalanis.web@adaits.es>
2024-06-09 19:30:16 +0200

    Added atributes to Cuenta, email and role

--- sistaleApi/src/models/cuenta-modelo.js
+++ sistaleApi/src/models/cuenta-modelo.js
@@ -4,6 +4,8 @@ import mongoose, { Schema } from 'mongoose';
 const cuentaModelo = new Schema({
   username: { type: String, required: true, unique: true, index: true },
   password: { type: String, required: true },
+  email: { type: String, required: true },
+  role: { type: String, default: 'miembro', enum: ['miembro', 'admin'], required: true },
 }, { collection: 'cuenta' });
 
 export default mongoose.model('Cuenta', cuentaModelo);
