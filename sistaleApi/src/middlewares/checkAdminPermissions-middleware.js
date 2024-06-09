7676bdf4b5769ca20716e31b1f896c4f90dc56a6
JoseLuisADA <joseluis.luzuriagaalanis.web@adaits.es>
2024-06-09 19:28:48 +0200

    New middleware checkAdminPermissions-middleware.js in SistaleApi

---
+++ sistaleApi/src/middlewares/checkAdminPermissions-middleware.js
@@ -0,0 +1,18 @@
+// src/middlewares/checkAdminPermissions-middleware.js
+import SistaleError from "../utils/SistaleError.js"
+import CuentaModel from '../models/cuenta-modelo.js';
+
+export async function checkAdminPermissions(req, res, next) {
+  const { username } = req.user
+
+  const user = await CuentaModel.findOne({ username })
+  
+  try {
+    if (!user) throw SistaleError.unauthorized("No autorizado")
+    if (user.role !== "admin") throw SistaleError.unauthorized("No autorizado")
+    next()
+  }
+  catch (error) {
+    return res.status(401).json({ message: error.message });
+  }
+}
